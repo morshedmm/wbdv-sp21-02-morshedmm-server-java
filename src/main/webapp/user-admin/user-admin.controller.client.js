04ee86882bd5b3e05ce86acabdeaa2682ee36fa3
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-02-05 10:22:54 -0500

    adding variable and functions names in cline.js

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,36 @@
+
+var $removeBtn, $editBtn, $createBtn;
+var $userRowTemplate, $tbody;
+var userService = new AdminUserServiceClient();
+
+
+function createUser() {
+
+}
+
+function deleteUser() {
+
+}
+function selectUser() {
+
+}
+
+function updateUser() {
+
+}
+
+function renderUsers(users) {
+
+}
+
+function findAllUsers() { } // optional - might not need this
+function findUserById() { } // optional - might not need this
+
+
+
+function main() {
+
+}
+
+jQuery(main)
+
