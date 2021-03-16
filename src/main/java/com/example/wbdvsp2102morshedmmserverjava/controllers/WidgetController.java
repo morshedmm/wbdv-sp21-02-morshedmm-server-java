c90dd5e8be8f279d534f2a9bef6fc1bb0e5ba8c6
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-03-16 11:40:56 -0400

    adding code for controller and services from Professor

--- src/main/java/com/example/wbdvsp2102morshedmmserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2102morshedmmserverjava/controllers/WidgetController.java
@@ -1,4 +1,38 @@
 package com.example.wbdvsp2102morshedmmserverjava.controllers;
 
+
+
+import com.example.wbdvsp2102morshedmmserverjava.models.Widget;
+import com.example.wbdvsp2102morshedmmserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class WidgetController {
+    @Autowired
+    WidgetService service;
+
+    @PostMapping("/api/topics/{tid}/widgets")
+    public Widget createWidgetForTopic(
+            @PathVariable("tid") String topicId,
+            @RequestBody Widget widget
+    ) {
+        return service.createWidgetForTopic(topicId, widget);
+    }
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(
+            @PathVariable("tid") String topicId
+    ) {
+        return service.findWidgetsForTopic(topicId);
+    }
 }
