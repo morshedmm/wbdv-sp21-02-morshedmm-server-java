0ce320cfaa28184456e17ffc7fb7a37dc810e0e9
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-03-19 19:29:06 -0400

    commented out defaults widgets

--- src/main/java/com/example/wbdvsp2102morshedmmserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2102morshedmmserverjava/services/WidgetService.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class WidgetService {
     private List<Widget> widgets = new ArrayList<Widget>();
     {
+        /**
         Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
         Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
         Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
@@ -21,6 +22,8 @@ public class WidgetService {
         widgets.add(w3);
         widgets.add(w4);
         widgets.add(w5);
+
+         */
     }
 
     public Widget createWidgetForTopic(String topicId, Widget widget) {
